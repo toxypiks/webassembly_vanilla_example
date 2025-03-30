a8c52ed2ab4afe3674a0e5d5b0a53f48d4606d30
toxypiks <HannaHinkelbein89@gmx.de>
2025-03-30 10:35:27 +0200

    Define function in wat script and export it to javascript file to let it get executed in browser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 async function start() {
     const wasm = await WebAssembly.instantiateStreaming(fetch("./hello.wasm"));
+    console.log(wasm.instance.exports.hello(34, 35));
     console.log(wasm);
 }
 
