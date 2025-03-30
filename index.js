c0ad34c78dc0c09029657393d680e2f8c5e8125b
toxypiks <HannaHinkelbein89@gmx.de>
2025-03-30 11:10:50 +0200

    Import javascript function into wat script

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 async function start() {
-    const wasm = await WebAssembly.instantiateStreaming(fetch("./hello.wasm"));
+    const wasm = await WebAssembly.instantiateStreaming(fetch("./hello.wasm"), {
+        "js": {
+            "print": (x) => console.log(`This function was called by WebAssembly program with argument ${x}`)
+        }
+    });
     console.log(wasm.instance.exports.hello(34, 35));
     console.log(wasm);
 }
